b9d4a8f463164bcadf8fef3d372ed308f7b4f8c7
Aditya Sharma <geovicco@gmail.com>
2021-12-22 04:08:16 +0530

    update app

--- app.py
+++ app.py
@@ -316,36 +316,62 @@ def addFVC(landsat):
   return wrap
 
 # 5. Surface Emissivity Calculation
-aster = ee.Image("NASA/ASTER_GED/AG100_003")
-#get ASTER FVC from NDVI
-aster_ndvi = aster.select('ndvi').multiply(0.01)
-aster_fvc = aster_ndvi.expression('((ndvi-ndvi_bg)/(ndvi_vg - ndvi_bg))**2', {'ndvi':aster_ndvi,'ndvi_bg':0.2,'ndvi_vg':0.86})
-aster_fvc = aster_fvc.where(aster_fvc.lt(0.0),0.0)
-aster_fvc = aster_fvc.where(aster_fvc.gt(1.0),1.0)
+# aster = ee.Image("NASA/ASTER_GED/AG100_003")
+# #get ASTER FVC from NDVI
+# aster_ndvi = aster.select('ndvi').multiply(0.01)
+# aster_fvc = aster_ndvi.expression('((ndvi-ndvi_bg)/(ndvi_vg - ndvi_bg))**2', {'ndvi':aster_ndvi,'ndvi_bg':0.2,'ndvi_vg':0.86})
+# aster_fvc = aster_fvc.where(aster_fvc.lt(0.0),0.0)
+# aster_fvc = aster_fvc.where(aster_fvc.gt(1.0),1.0)
 
 # bare ground emissivity functions for each band
 def add_emiss_bare_band10(image):
-  return image.expression('(EM - 0.99*fvc)/(1.0-fvc)',{
+    aster = ee.Image("NASA/ASTER_GED/AG100_003")
+    aster_ndvi = aster.select('ndvi').multiply(0.01)
+    aster_fvc = aster_ndvi.expression('((ndvi-ndvi_bg)/(ndvi_vg - ndvi_bg))**2', {'ndvi':aster_ndvi,'ndvi_bg':0.2,'ndvi_vg':0.86})
+    aster_fvc = aster_fvc.where(aster_fvc.lt(0.0),0.0)
+    aster_fvc = aster_fvc.where(aster_fvc.gt(1.0),1.0)
+    
+    return image.expression('(EM - 0.99*fvc)/(1.0-fvc)',{
     'EM':aster.select('emissivity_band10').multiply(0.001),
     'fvc':aster_fvc}).clip(image.geometry())
 
 def add_emiss_bare_band11(image):
-  return image.expression('(EM - 0.99*fvc)/(1.0-fvc)',{
+    aster = ee.Image("NASA/ASTER_GED/AG100_003")
+    aster_ndvi = aster.select('ndvi').multiply(0.01)
+    aster_fvc = aster_ndvi.expression('((ndvi-ndvi_bg)/(ndvi_vg - ndvi_bg))**2', {'ndvi':aster_ndvi,'ndvi_bg':0.2,'ndvi_vg':0.86})
+    aster_fvc = aster_fvc.where(aster_fvc.lt(0.0),0.0)
+    aster_fvc = aster_fvc.where(aster_fvc.gt(1.0),1.0)  
+    return image.expression('(EM - 0.99*fvc)/(1.0-fvc)',{
     'EM':aster.select('emissivity_band11').multiply(0.001),
     'fvc':aster_fvc}).clip(image.geometry())
 
 def add_emiss_bare_band12(image):
-  return image.expression('(EM - 0.99*fvc)/(1.0-fvc)',{
+    aster = ee.Image("NASA/ASTER_GED/AG100_003")
+    aster_ndvi = aster.select('ndvi').multiply(0.01)
+    aster_fvc = aster_ndvi.expression('((ndvi-ndvi_bg)/(ndvi_vg - ndvi_bg))**2', {'ndvi':aster_ndvi,'ndvi_bg':0.2,'ndvi_vg':0.86})
+    aster_fvc = aster_fvc.where(aster_fvc.lt(0.0),0.0)
+    aster_fvc = aster_fvc.where(aster_fvc.gt(1.0),1.0)
+    return image.expression('(EM - 0.99*fvc)/(1.0-fvc)',{
     'EM':aster.select('emissivity_band12').multiply(0.001),
     'fvc':aster_fvc}).clip(image.geometry())
 
 def add_emiss_bare_band13(image):
-  return image.expression('(EM - 0.99*fvc)/(1.0-fvc)',{
+    aster = ee.Image("NASA/ASTER_GED/AG100_003")
+    aster_ndvi = aster.select('ndvi').multiply(0.01)
+    aster_fvc = aster_ndvi.expression('((ndvi-ndvi_bg)/(ndvi_vg - ndvi_bg))**2', {'ndvi':aster_ndvi,'ndvi_bg':0.2,'ndvi_vg':0.86})
+    aster_fvc = aster_fvc.where(aster_fvc.lt(0.0),0.0)
+    aster_fvc = aster_fvc.where(aster_fvc.gt(1.0),1.0)
+    return image.expression('(EM - 0.99*fvc)/(1.0-fvc)',{
     'EM':aster.select('emissivity_band13').multiply(0.001),
     'fvc':aster_fvc}).clip(image.geometry())
 
 def add_emiss_bare_band14(image):
-  return image.expression('(EM - 0.99*fvc)/(1.0-fvc)',{
+    aster = ee.Image("NASA/ASTER_GED/AG100_003")
+    aster_ndvi = aster.select('ndvi').multiply(0.01)
+    aster_fvc = aster_ndvi.expression('((ndvi-ndvi_bg)/(ndvi_vg - ndvi_bg))**2', {'ndvi':aster_ndvi,'ndvi_bg':0.2,'ndvi_vg':0.86})
+    aster_fvc = aster_fvc.where(aster_fvc.lt(0.0),0.0)
+    aster_fvc = aster_fvc.where(aster_fvc.gt(1.0),1.0)
+    return image.expression('(EM - 0.99*fvc)/(1.0-fvc)',{
     'EM':aster.select('emissivity_band14').multiply(0.001),
     'fvc':aster_fvc}).clip(image.geometry())
 
--- setup.sh
+++ setup.sh
@@ -1,9 +1,8 @@
 mkdir -p ~/.streamlit/
-echo "[general]
-email = \"geovicco@gmail.com\"
-" > ~/.streamlit/credentials.toml
-echo "[server]
-headless = true
-port = $PORT
-enableCORS = true
+echo "\
+[server]\n\
+headless = true\n\
+port = $PORT\n\
+enableCORS = false\n\
+\n\
 " > ~/.streamlit/config.toml
