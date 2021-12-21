4d9b0eeb0ed87b12a36fc6003623a52d5fc7ea58
Aditya Sharma <geovicco@gmail.com>
2021-12-22 02:26:25 +0530

    updated

--- setup.sh
+++
@@ -1,9 +0,0 @@
-mkdir -p ~/.streamlit/
-echo "[general]
-email = \"geovicco@gmail.com\"
-" > ~/.streamlit/credentials.toml
-echo "[server]
-headless = true
-port = $PORT
-enableCORS = false
-" > ~/.streamlit/config.toml
