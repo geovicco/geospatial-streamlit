8c2833a0de8584c232824d9099cfbc7bb3035b2b
Aditya Sharma <geovicco@gmail.com>
2021-12-22 14:00:04 +0530

    Update lst.py

--- lst.py
+++ lst.py
@@ -525,60 +525,59 @@ def addLST(landsat):
     return image.addBands(lst.rename('LST'))
   return wrap
 
-
-COLLECTION = ee.Dictionary({
-  'Landsat 4': {
-    'TOA': ee.ImageCollection('LANDSAT/LT04/C01/T1_TOA'),
-    'SR': ee.ImageCollection('LANDSAT/LT04/C01/T1_SR'),
-    'TIR': ['B6',]
-  },
-  'Landsat 5': {
-    'TOA': ee.ImageCollection('LANDSAT/LT05/C01/T1_TOA'),
-    'SR': ee.ImageCollection('LANDSAT/LT05/C01/T1_SR'),
-    'TIR': ['B6',]
-  },
-  'Landsat 7': {
-    'TOA': ee.ImageCollection('LANDSAT/LE07/C01/T1_TOA'),
-    'SR': ee.ImageCollection('LANDSAT/LE07/C01/T1_SR'),
-    'TIR': ['B6_VCID_1','B6_VCID_2'],
-  },
-  'Landsat 8': {
-    'TOA': ee.ImageCollection('LANDSAT/LC08/C01/T1_TOA'),
-    'SR': ee.ImageCollection('LANDSAT/LC08/C01/T1_SR'),
-    'TIR': ['B10','B11']
-  }
-})
-
 def getLSTCollection(landsat, date_start, date_end, geometry, use_ndvi):
+    
+    COLLECTION = ee.Dictionary({
+        'Landsat 4': {
+        'TOA': ee.ImageCollection('LANDSAT/LT04/C01/T1_TOA'),
+        'SR': ee.ImageCollection('LANDSAT/LT04/C01/T1_SR'),
+        'TIR': ['B6',]
+        },
+        'Landsat 5': {
+            'TOA': ee.ImageCollection('LANDSAT/LT05/C01/T1_TOA'),
+            'SR': ee.ImageCollection('LANDSAT/LT05/C01/T1_SR'),
+            'TIR': ['B6',]
+        },
+        'Landsat 7': {
+            'TOA': ee.ImageCollection('LANDSAT/LE07/C01/T1_TOA'),
+            'SR': ee.ImageCollection('LANDSAT/LE07/C01/T1_SR'),
+            'TIR': ['B6_VCID_1','B6_VCID_2'],
+        },
+        'Landsat 8': {
+            'TOA': ee.ImageCollection('LANDSAT/LC08/C01/T1_TOA'),
+            'SR': ee.ImageCollection('LANDSAT/LC08/C01/T1_SR'),
+            'TIR': ['B10','B11']
+        }
+    })
 
-  # load TOA Radiance/Reflectance
-  collection_dict = ee.Dictionary(COLLECTION.get(landsat))
+    # load TOA Radiance/Reflectance
+    collection_dict = ee.Dictionary(COLLECTION.get(landsat))
 
-  landsatTOA = ee.ImageCollection(collection_dict.get('TOA')) \
-                .filter(ee.Filter.date(date_start, date_end)) \
-                .filterBounds(geometry) \
-                .map(maskL8toaClouds)
+    landsatTOA = ee.ImageCollection(collection_dict.get('TOA')) \
+                    .filter(ee.Filter.date(date_start, date_end)) \
+                    .filterBounds(geometry) \
+                    .map(maskL8toaClouds)
 
-  # load Surface Reflectance collection for NDVI
-  landsatSR = ee.ImageCollection(collection_dict.get('SR')) \
-                .filter(ee.Filter.date(date_start, date_end)) \
-                .filterBounds(geometry) \
-                .map(maskL8srClouds) \
-                .map(addNDVI(landsat)) \
-                .map(addFVC(landsat)) \
-                .map(addTPWBands) \
-                .map(addEM(landsat,use_ndvi))
+    # load Surface Reflectance collection for NDVI
+    landsatSR = ee.ImageCollection(collection_dict.get('SR')) \
+                    .filter(ee.Filter.date(date_start, date_end)) \
+                    .filterBounds(geometry) \
+                    .map(maskL8srClouds) \
+                    .map(addNDVI(landsat)) \
+                    .map(addFVC(landsat)) \
+                    .map(addTPWBands) \
+                    .map(addEM(landsat,use_ndvi))
 
-  # combine collections
-  # all channels from surface reflectance collection
-  # except tir channels: from TOA collection
-  # select TIR bands
-  tir = ee.List(collection_dict.get('TIR'))
-  landsatALL = (landsatSR.combine(landsatTOA.select(tir),True))
+    # combine collections
+    # all channels from surface reflectance collection
+    # except tir channels: from TOA collection
+    # select TIR bands
+    tir = ee.List(collection_dict.get('TIR'))
+    landsatALL = (landsatSR.combine(landsatTOA.select(tir),True))
 
-  # compute the LST
-  landsatLST = landsatALL.map(addLST(landsat))
-  return landsatLST
+    # compute the LST
+    landsatLST = landsatALL.map(addLST(landsat))
+    return landsatLST
 ################################################################################
 ############## END of SMW LST ALGORITHM ########################################
 
