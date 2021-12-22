db5ce598f17bd851d09494fda5ab4df14c02bc72
Aditya Sharma <geovicco@gmail.com>
2021-12-22 14:05:46 +0530

    updates lst.py

--- utilities.py
+++ utilities.py
@@ -316,13 +316,6 @@ def addFVC(landsat):
   return wrap
 
 # 5. Surface Emissivity Calculation
-# aster = ee.Image("NASA/ASTER_GED/AG100_003")
-# #get ASTER FVC from NDVI
-# aster_ndvi = aster.select('ndvi').multiply(0.01)
-# aster_fvc = aster_ndvi.expression('((ndvi-ndvi_bg)/(ndvi_vg - ndvi_bg))**2', {'ndvi':aster_ndvi,'ndvi_bg':0.2,'ndvi_vg':0.86})
-# aster_fvc = aster_fvc.where(aster_fvc.lt(0.0),0.0)
-# aster_fvc = aster_fvc.where(aster_fvc.gt(1.0),1.0)
-
 # bare ground emissivity functions for each band
 def add_emiss_bare_band10(image):
     aster = ee.Image("NASA/ASTER_GED/AG100_003")
@@ -524,60 +517,59 @@ def addLST(landsat):
     return image.addBands(lst.rename('LST'))
   return wrap
 
-
-COLLECTION = ee.Dictionary({
-  'Landsat 4': {
-    'TOA': ee.ImageCollection('LANDSAT/LT04/C01/T1_TOA'),
-    'SR': ee.ImageCollection('LANDSAT/LT04/C01/T1_SR'),
-    'TIR': ['B6',]
-  },
-  'Landsat 5': {
-    'TOA': ee.ImageCollection('LANDSAT/LT05/C01/T1_TOA'),
-    'SR': ee.ImageCollection('LANDSAT/LT05/C01/T1_SR'),
-    'TIR': ['B6',]
-  },
-  'Landsat 7': {
-    'TOA': ee.ImageCollection('LANDSAT/LE07/C01/T1_TOA'),
-    'SR': ee.ImageCollection('LANDSAT/LE07/C01/T1_SR'),
-    'TIR': ['B6_VCID_1','B6_VCID_2'],
-  },
-  'Landsat 8': {
-    'TOA': ee.ImageCollection('LANDSAT/LC08/C01/T1_TOA'),
-    'SR': ee.ImageCollection('LANDSAT/LC08/C01/T1_SR'),
-    'TIR': ['B10','B11']
-  }
-})
-
 def getLSTCollection(landsat, date_start, date_end, geometry, use_ndvi):
+    
+    COLLECTION = ee.Dictionary({
+        'Landsat 4': {
+        'TOA': ee.ImageCollection('LANDSAT/LT04/C01/T1_TOA'),
+        'SR': ee.ImageCollection('LANDSAT/LT04/C01/T1_SR'),
+        'TIR': ['B6',]
+        },
+        'Landsat 5': {
+            'TOA': ee.ImageCollection('LANDSAT/LT05/C01/T1_TOA'),
+            'SR': ee.ImageCollection('LANDSAT/LT05/C01/T1_SR'),
+            'TIR': ['B6',]
+        },
+        'Landsat 7': {
+            'TOA': ee.ImageCollection('LANDSAT/LE07/C01/T1_TOA'),
+            'SR': ee.ImageCollection('LANDSAT/LE07/C01/T1_SR'),
+            'TIR': ['B6_VCID_1','B6_VCID_2'],
+        },
+        'Landsat 8': {
+            'TOA': ee.ImageCollection('LANDSAT/LC08/C01/T1_TOA'),
+            'SR': ee.ImageCollection('LANDSAT/LC08/C01/T1_SR'),
+            'TIR': ['B10','B11']
+        }
+    })
 
-  # load TOA Radiance/Reflectance
-  collection_dict = ee.Dictionary(COLLECTION.get(landsat))
+    # load TOA Radiance/Reflectance
+    collection_dict = ee.Dictionary(COLLECTION.get(landsat))
 
-  landsatTOA = ee.ImageCollection(collection_dict.get('TOA')) \
-                .filter(ee.Filter.date(date_start, date_end)) \
-                .filterBounds(geometry) \
-                .map(maskL8toaClouds)
+    landsatTOA = ee.ImageCollection(collection_dict.get('TOA')) \
+                    .filter(ee.Filter.date(date_start, date_end)) \
+                    .filterBounds(geometry) \
+                    .map(maskL8toaClouds)
 
-  # load Surface Reflectance collection for NDVI
-  landsatSR = ee.ImageCollection(collection_dict.get('SR')) \
-                .filter(ee.Filter.date(date_start, date_end)) \
-                .filterBounds(geometry) \
-                .map(maskL8srClouds) \
-                .map(addNDVI(landsat)) \
-                .map(addFVC(landsat)) \
-                .map(addTPWBands) \
-                .map(addEM(landsat,use_ndvi))
+    # load Surface Reflectance collection for NDVI
+    landsatSR = ee.ImageCollection(collection_dict.get('SR')) \
+                    .filter(ee.Filter.date(date_start, date_end)) \
+                    .filterBounds(geometry) \
+                    .map(maskL8srClouds) \
+                    .map(addNDVI(landsat)) \
+                    .map(addFVC(landsat)) \
+                    .map(addTPWBands) \
+                    .map(addEM(landsat,use_ndvi))
 
-  # combine collections
-  # all channels from surface reflectance collection
-  # except tir channels: from TOA collection
-  # select TIR bands
-  tir = ee.List(collection_dict.get('TIR'))
-  landsatALL = (landsatSR.combine(landsatTOA.select(tir),True))
+    # combine collections
+    # all channels from surface reflectance collection
+    # except tir channels: from TOA collection
+    # select TIR bands
+    tir = ee.List(collection_dict.get('TIR'))
+    landsatALL = (landsatSR.combine(landsatTOA.select(tir),True))
 
-  # compute the LST
-  landsatLST = landsatALL.map(addLST(landsat))
-  return landsatLST
+    # compute the LST
+    landsatLST = landsatALL.map(addLST(landsat))
+    return landsatLST
 ################################################################################
 ############## END of SMW LST ALGORITHM ########################################
 
