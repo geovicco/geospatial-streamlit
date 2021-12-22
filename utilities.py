3fe9abf3f005a10e0e9ed13bb089c6513a25be83
Aditya Sharma <geovicco@gmail.com>
2021-12-22 13:47:32 +0530

    renamed and updated test.py to lst.py

--- utilities.py
+++ utilities.py
@@ -6,11 +6,6 @@ import folium
 import geopandas as gpd
 from datetime import date, timedelta
 
-# import os
-# print(os.environ.get("EARTHENGINE_TOKEN"))
-# token = os.environ.get('EARTHENGINE_TOKEN')
-# geemap.ee_initialize(token)
-
 def initialize_sessionState():
     if st.session_state.get("zoom_level") is None:
         st.session_state["zoom_level"] = 3
@@ -218,7 +213,8 @@ def set_params():
             st.session_state['satellite'] = satellite
             
         return st.session_state
-
+    
+    
 ################################################################################
 ############## LST Single Mono Window Algorithm ################################
 # 1. Mask Clouds from TOA and SR Landsat Collection
@@ -320,36 +316,62 @@ def addFVC(landsat):
   return wrap
 
 # 5. Surface Emissivity Calculation
-aster = ee.Image("NASA/ASTER_GED/AG100_003")
-#get ASTER FVC from NDVI
-aster_ndvi = aster.select('ndvi').multiply(0.01)
-aster_fvc = aster_ndvi.expression('((ndvi-ndvi_bg)/(ndvi_vg - ndvi_bg))**2', {'ndvi':aster_ndvi,'ndvi_bg':0.2,'ndvi_vg':0.86})
-aster_fvc = aster_fvc.where(aster_fvc.lt(0.0),0.0)
-aster_fvc = aster_fvc.where(aster_fvc.gt(1.0),1.0)
+# aster = ee.Image("NASA/ASTER_GED/AG100_003")
+# #get ASTER FVC from NDVI
+# aster_ndvi = aster.select('ndvi').multiply(0.01)
+# aster_fvc = aster_ndvi.expression('((ndvi-ndvi_bg)/(ndvi_vg - ndvi_bg))**2', {'ndvi':aster_ndvi,'ndvi_bg':0.2,'ndvi_vg':0.86})
+# aster_fvc = aster_fvc.where(aster_fvc.lt(0.0),0.0)
+# aster_fvc = aster_fvc.where(aster_fvc.gt(1.0),1.0)
 
 # bare ground emissivity functions for each band
 def add_emiss_bare_band10(image):
-  return image.expression('(EM - 0.99*fvc)/(1.0-fvc)',{
+    aster = ee.Image("NASA/ASTER_GED/AG100_003")
+    aster_ndvi = aster.select('ndvi').multiply(0.01)
+    aster_fvc = aster_ndvi.expression('((ndvi-ndvi_bg)/(ndvi_vg - ndvi_bg))**2', {'ndvi':aster_ndvi,'ndvi_bg':0.2,'ndvi_vg':0.86})
+    aster_fvc = aster_fvc.where(aster_fvc.lt(0.0),0.0)
+    aster_fvc = aster_fvc.where(aster_fvc.gt(1.0),1.0)
+    
+    return image.expression('(EM - 0.99*fvc)/(1.0-fvc)',{
     'EM':aster.select('emissivity_band10').multiply(0.001),
     'fvc':aster_fvc}).clip(image.geometry())
 
 def add_emiss_bare_band11(image):
-  return image.expression('(EM - 0.99*fvc)/(1.0-fvc)',{
+    aster = ee.Image("NASA/ASTER_GED/AG100_003")
+    aster_ndvi = aster.select('ndvi').multiply(0.01)
+    aster_fvc = aster_ndvi.expression('((ndvi-ndvi_bg)/(ndvi_vg - ndvi_bg))**2', {'ndvi':aster_ndvi,'ndvi_bg':0.2,'ndvi_vg':0.86})
+    aster_fvc = aster_fvc.where(aster_fvc.lt(0.0),0.0)
+    aster_fvc = aster_fvc.where(aster_fvc.gt(1.0),1.0)  
+    return image.expression('(EM - 0.99*fvc)/(1.0-fvc)',{
     'EM':aster.select('emissivity_band11').multiply(0.001),
     'fvc':aster_fvc}).clip(image.geometry())
 
 def add_emiss_bare_band12(image):
-  return image.expression('(EM - 0.99*fvc)/(1.0-fvc)',{
+    aster = ee.Image("NASA/ASTER_GED/AG100_003")
+    aster_ndvi = aster.select('ndvi').multiply(0.01)
+    aster_fvc = aster_ndvi.expression('((ndvi-ndvi_bg)/(ndvi_vg - ndvi_bg))**2', {'ndvi':aster_ndvi,'ndvi_bg':0.2,'ndvi_vg':0.86})
+    aster_fvc = aster_fvc.where(aster_fvc.lt(0.0),0.0)
+    aster_fvc = aster_fvc.where(aster_fvc.gt(1.0),1.0)
+    return image.expression('(EM - 0.99*fvc)/(1.0-fvc)',{
     'EM':aster.select('emissivity_band12').multiply(0.001),
     'fvc':aster_fvc}).clip(image.geometry())
 
 def add_emiss_bare_band13(image):
-  return image.expression('(EM - 0.99*fvc)/(1.0-fvc)',{
+    aster = ee.Image("NASA/ASTER_GED/AG100_003")
+    aster_ndvi = aster.select('ndvi').multiply(0.01)
+    aster_fvc = aster_ndvi.expression('((ndvi-ndvi_bg)/(ndvi_vg - ndvi_bg))**2', {'ndvi':aster_ndvi,'ndvi_bg':0.2,'ndvi_vg':0.86})
+    aster_fvc = aster_fvc.where(aster_fvc.lt(0.0),0.0)
+    aster_fvc = aster_fvc.where(aster_fvc.gt(1.0),1.0)
+    return image.expression('(EM - 0.99*fvc)/(1.0-fvc)',{
     'EM':aster.select('emissivity_band13').multiply(0.001),
     'fvc':aster_fvc}).clip(image.geometry())
 
 def add_emiss_bare_band14(image):
-  return image.expression('(EM - 0.99*fvc)/(1.0-fvc)',{
+    aster = ee.Image("NASA/ASTER_GED/AG100_003")
+    aster_ndvi = aster.select('ndvi').multiply(0.01)
+    aster_fvc = aster_ndvi.expression('((ndvi-ndvi_bg)/(ndvi_vg - ndvi_bg))**2', {'ndvi':aster_ndvi,'ndvi_bg':0.2,'ndvi_vg':0.86})
+    aster_fvc = aster_fvc.where(aster_fvc.lt(0.0),0.0)
+    aster_fvc = aster_fvc.where(aster_fvc.gt(1.0),1.0)
+    return image.expression('(EM - 0.99*fvc)/(1.0-fvc)',{
     'EM':aster.select('emissivity_band14').multiply(0.001),
     'fvc':aster_fvc}).clip(image.geometry())
 
@@ -405,57 +427,61 @@ def addEM(landsat, use_ndvi):
 
 # 6. SWM Algorithm for Calculating LST
 # coefficients for the Statistical Mono-Window Algorithm
-coeff_SMW_L4 = ee.FeatureCollection([
-  ee.Feature(None, {'TPWpos': 0, 'A': 0.9755, 'B': -205.2767, 'C': 212.0051}),
-  ee.Feature(None, {'TPWpos': 1, 'A': 1.0155, 'B': -233.8902, 'C': 230.4049}),
-  ee.Feature(None, {'TPWpos': 2, 'A': 1.0672, 'B': -257.1884, 'C': 239.3072}),
-  ee.Feature(None, {'TPWpos': 3, 'A': 1.1499, 'B': -286.2166, 'C': 244.8497}),
-  ee.Feature(None, {'TPWpos': 4, 'A': 1.2277, 'B': -316.7643, 'C': 253.0033}),
-  ee.Feature(None, {'TPWpos': 5, 'A': 1.3649, 'B': -361.8276, 'C': 258.5471}),
-  ee.Feature(None, {'TPWpos': 6, 'A': 1.5085, 'B': -410.1157, 'C': 265.1131}),
-  ee.Feature(None, {'TPWpos': 7, 'A': 1.7045, 'B': -472.4909, 'C': 270.7000}),
-  ee.Feature(None, {'TPWpos': 8, 'A': 1.5886, 'B': -442.9489, 'C': 277.1511}),
-  ee.Feature(None, {'TPWpos': 9, 'A': 2.0215, 'B': -571.8563, 'C': 279.9854})
-])
+def coeff_SMW_L4():
+    return ee.FeatureCollection([
+    ee.Feature(None, {'TPWpos': 0, 'A': 0.9755, 'B': -205.2767, 'C': 212.0051}),
+    ee.Feature(None, {'TPWpos': 1, 'A': 1.0155, 'B': -233.8902, 'C': 230.4049}),
+    ee.Feature(None, {'TPWpos': 2, 'A': 1.0672, 'B': -257.1884, 'C': 239.3072}),
+    ee.Feature(None, {'TPWpos': 3, 'A': 1.1499, 'B': -286.2166, 'C': 244.8497}),
+    ee.Feature(None, {'TPWpos': 4, 'A': 1.2277, 'B': -316.7643, 'C': 253.0033}),
+    ee.Feature(None, {'TPWpos': 5, 'A': 1.3649, 'B': -361.8276, 'C': 258.5471}),
+    ee.Feature(None, {'TPWpos': 6, 'A': 1.5085, 'B': -410.1157, 'C': 265.1131}),
+    ee.Feature(None, {'TPWpos': 7, 'A': 1.7045, 'B': -472.4909, 'C': 270.7000}),
+    ee.Feature(None, {'TPWpos': 8, 'A': 1.5886, 'B': -442.9489, 'C': 277.1511}),
+    ee.Feature(None, {'TPWpos': 9, 'A': 2.0215, 'B': -571.8563, 'C': 279.9854})
+    ])
 
-coeff_SMW_L5 = ee.FeatureCollection([
-  ee.Feature(None, {'TPWpos': 0, 'A': 0.9765, 'B': -204.6584, 'C': 211.1321}),
-  ee.Feature(None, {'TPWpos': 1, 'A': 1.0229, 'B': -235.5384, 'C': 230.0619}),
-  ee.Feature(None, {'TPWpos': 2, 'A': 1.0817, 'B': -261.3886, 'C': 239.5256}),
-  ee.Feature(None, {'TPWpos': 3, 'A': 1.1738, 'B': -293.6128, 'C': 245.6042}),
-  ee.Feature(None, {'TPWpos': 4, 'A': 1.2605, 'B': -327.1417, 'C': 254.2301}),
-  ee.Feature(None, {'TPWpos': 5, 'A': 1.4166, 'B': -377.7741, 'C': 259.9711}),
-  ee.Feature(None, {'TPWpos': 6, 'A': 1.5727, 'B': -430.0388, 'C': 266.9520}),
-  ee.Feature(None, {'TPWpos': 7, 'A': 1.7879, 'B': -498.1947, 'C': 272.8413}),
-  ee.Feature(None, {'TPWpos': 8, 'A': 1.6347, 'B': -457.8183, 'C': 279.6160}),
-  ee.Feature(None, {'TPWpos': 9, 'A': 2.1168, 'B': -600.7079, 'C': 282.4583})
-])
+def coeff_SMW_L5():
+    return ee.FeatureCollection([
+    ee.Feature(None, {'TPWpos': 0, 'A': 0.9765, 'B': -204.6584, 'C': 211.1321}),
+    ee.Feature(None, {'TPWpos': 1, 'A': 1.0229, 'B': -235.5384, 'C': 230.0619}),
+    ee.Feature(None, {'TPWpos': 2, 'A': 1.0817, 'B': -261.3886, 'C': 239.5256}),
+    ee.Feature(None, {'TPWpos': 3, 'A': 1.1738, 'B': -293.6128, 'C': 245.6042}),
+    ee.Feature(None, {'TPWpos': 4, 'A': 1.2605, 'B': -327.1417, 'C': 254.2301}),
+    ee.Feature(None, {'TPWpos': 5, 'A': 1.4166, 'B': -377.7741, 'C': 259.9711}),
+    ee.Feature(None, {'TPWpos': 6, 'A': 1.5727, 'B': -430.0388, 'C': 266.9520}),
+    ee.Feature(None, {'TPWpos': 7, 'A': 1.7879, 'B': -498.1947, 'C': 272.8413}),
+    ee.Feature(None, {'TPWpos': 8, 'A': 1.6347, 'B': -457.8183, 'C': 279.6160}),
+    ee.Feature(None, {'TPWpos': 9, 'A': 2.1168, 'B': -600.7079, 'C': 282.4583})
+    ])
 
-coeff_SMW_L7 = ee.FeatureCollection([
-  ee.Feature(None, {'TPWpos': 0, 'A': 0.9764, 'B': -205.3511, 'C': 211.8507}),
-  ee.Feature(None, {'TPWpos': 1, 'A': 1.0201, 'B': -235.2416, 'C': 230.5468}),
-  ee.Feature(None, {'TPWpos': 2, 'A': 1.0750, 'B': -259.6560, 'C': 239.6619}),
-  ee.Feature(None, {'TPWpos': 3, 'A': 1.1612, 'B': -289.8190, 'C': 245.3286}),
-  ee.Feature(None, {'TPWpos': 4, 'A': 1.2425, 'B': -321.4658, 'C': 253.6144}),
-  ee.Feature(None, {'TPWpos': 5, 'A': 1.3864, 'B': -368.4078, 'C': 259.1390}),
-  ee.Feature(None, {'TPWpos': 6, 'A': 1.5336, 'B': -417.7796, 'C': 265.7486}),
-  ee.Feature(None, {'TPWpos': 7, 'A': 1.7345, 'B': -481.5714, 'C': 271.3659}),
-  ee.Feature(None, {'TPWpos': 8, 'A': 1.6066, 'B': -448.5071, 'C': 277.9058}),
-  ee.Feature(None, {'TPWpos': 9, 'A': 2.0533, 'B': -581.2619, 'C': 280.6800})
-])
+def coeff_SMW_L7():
+    return ee.FeatureCollection([
+    ee.Feature(None, {'TPWpos': 0, 'A': 0.9764, 'B': -205.3511, 'C': 211.8507}),
+    ee.Feature(None, {'TPWpos': 1, 'A': 1.0201, 'B': -235.2416, 'C': 230.5468}),
+    ee.Feature(None, {'TPWpos': 2, 'A': 1.0750, 'B': -259.6560, 'C': 239.6619}),
+    ee.Feature(None, {'TPWpos': 3, 'A': 1.1612, 'B': -289.8190, 'C': 245.3286}),
+    ee.Feature(None, {'TPWpos': 4, 'A': 1.2425, 'B': -321.4658, 'C': 253.6144}),
+    ee.Feature(None, {'TPWpos': 5, 'A': 1.3864, 'B': -368.4078, 'C': 259.1390}),
+    ee.Feature(None, {'TPWpos': 6, 'A': 1.5336, 'B': -417.7796, 'C': 265.7486}),
+    ee.Feature(None, {'TPWpos': 7, 'A': 1.7345, 'B': -481.5714, 'C': 271.3659}),
+    ee.Feature(None, {'TPWpos': 8, 'A': 1.6066, 'B': -448.5071, 'C': 277.9058}),
+    ee.Feature(None, {'TPWpos': 9, 'A': 2.0533, 'B': -581.2619, 'C': 280.6800})
+    ])
 
-coeff_SMW_L8 = ee.FeatureCollection([
-  ee.Feature(None, {'TPWpos': 0, 'A': 0.9751, 'B': -205.8929, 'C': 212.7173}),
-  ee.Feature(None, {'TPWpos': 1, 'A': 1.0090, 'B': -232.2750, 'C': 230.5698}),
-  ee.Feature(None, {'TPWpos': 2, 'A': 1.0541, 'B': -253.1943, 'C': 238.9548}),
-  ee.Feature(None, {'TPWpos': 3, 'A': 1.1282, 'B': -279.4212, 'C': 244.0772}),
-  ee.Feature(None, {'TPWpos': 4, 'A': 1.1987, 'B': -307.4497, 'C': 251.8341}),
-  ee.Feature(None, {'TPWpos': 5, 'A': 1.3205, 'B': -348.0228, 'C': 257.2740}),
-  ee.Feature(None, {'TPWpos': 6, 'A': 1.4540, 'B': -393.1718, 'C': 263.5599}),
-  ee.Feature(None, {'TPWpos': 7, 'A': 1.6350, 'B': -451.0790, 'C': 268.9405}),
-  ee.Feature(None, {'TPWpos': 8, 'A': 1.5468, 'B': -429.5095, 'C': 275.0895}),
-  ee.Feature(None, {'TPWpos': 9, 'A': 1.9403, 'B': -547.2681, 'C': 277.9953})
-])
+def coeff_SMW_L8():
+    return ee.FeatureCollection([
+    ee.Feature(None, {'TPWpos': 0, 'A': 0.9751, 'B': -205.8929, 'C': 212.7173}),
+    ee.Feature(None, {'TPWpos': 1, 'A': 1.0090, 'B': -232.2750, 'C': 230.5698}),
+    ee.Feature(None, {'TPWpos': 2, 'A': 1.0541, 'B': -253.1943, 'C': 238.9548}),
+    ee.Feature(None, {'TPWpos': 3, 'A': 1.1282, 'B': -279.4212, 'C': 244.0772}),
+    ee.Feature(None, {'TPWpos': 4, 'A': 1.1987, 'B': -307.4497, 'C': 251.8341}),
+    ee.Feature(None, {'TPWpos': 5, 'A': 1.3205, 'B': -348.0228, 'C': 257.2740}),
+    ee.Feature(None, {'TPWpos': 6, 'A': 1.4540, 'B': -393.1718, 'C': 263.5599}),
+    ee.Feature(None, {'TPWpos': 7, 'A': 1.6350, 'B': -451.0790, 'C': 268.9405}),
+    ee.Feature(None, {'TPWpos': 8, 'A': 1.5468, 'B': -429.5095, 'C': 275.0895}),
+    ee.Feature(None, {'TPWpos': 9, 'A': 1.9403, 'B': -547.2681, 'C': 277.9953})
+    ])
 
 # Function to create a lookup between two columns in a
 # feature collection
@@ -467,10 +493,10 @@ def get_lookup_table(fc, prop_1, prop_2):
 def addLST(landsat):
   def wrap(image):
     # Select algorithm coefficients
-    coeff_SMW = ee.FeatureCollection(ee.Algorithms.If(landsat=='Landsat 4',coeff_SMW_L4,
-                                        ee.Algorithms.If(landsat=='Landsat 5',coeff_SMW_L5,
-                                        ee.Algorithms.If(landsat=='Landsat 7',coeff_SMW_L7,
-                                        coeff_SMW_L8))))
+    coeff_SMW = ee.FeatureCollection(ee.Algorithms.If(landsat=='Landsat 4',coeff_SMW_L4(),
+                                        ee.Algorithms.If(landsat=='Landsat 5',coeff_SMW_L5(),
+                                        ee.Algorithms.If(landsat=='Landsat 7',coeff_SMW_L7(),
+                                        coeff_SMW_L8()))))
 
     # Create lookups for the algorithm coefficients
     A_lookup = get_lookup_table(coeff_SMW, 'TPWpos', 'A')
@@ -582,4 +608,3 @@ def showLST(mapObject, state):
     caption = 'Land Surface Temperature (Celsius)'
 
     mapObject.add_colorbar(colors=cmap1, vmin=vmin, vmax=round(vmax, 2), caption=caption)
-    
