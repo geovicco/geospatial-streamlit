de306c78adbcbe92cb58ca70dcc621e6b867d882
Aditya Sharma <geovicco@gmail.com>
2021-12-23 09:31:53 +0530

    Update lst.py

--- lst.py
+++ lst.py
@@ -44,8 +44,8 @@ with row1_col1:
                     # Run Algorithm
                     ut.showLST(mapObject=m, state=sessionState)
                 except KeyError:
+                    st.sessionState['FormSubmitter:processing-params-Submit'] = False
                     st.warning("LST for selected dates could not be computed. Set longer time duration!")
-                    sessionState['FormSubmitter:processing-params-Submit'] == False
             else:
                 st.info("Define Processing Paramaters to Proceed")        
 
